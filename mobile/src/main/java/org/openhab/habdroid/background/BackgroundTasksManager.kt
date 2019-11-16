07a0e11eb3f8cb0e309175dedcbb395309136053
glebon_1 <glebon_1@yahoo.com>
2019-11-16 16:15:21 +0200

    Adding UI for enhancement

--- mobile/src/main/java/org/openhab/habdroid/background/BackgroundTasksManager.kt
+++ mobile/src/main/java/org/openhab/habdroid/background/BackgroundTasksManager.kt
@@ -19,7 +19,6 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
-import android.net.wifi.WifiManager
 import android.os.Build
 import android.os.Parcelable
 import android.telephony.TelephonyManager
